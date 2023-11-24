f603adec4586c020c2daf2ef2315f76773d90398
isinhag <isabeladasilvagomes@gmail.com>
2023-11-24 15:59:19 -0300

    Correções

--- js/confirm.js
+++ js/confirm.js
@@ -2,12 +2,12 @@
 const links = document.querySelectorAll('.excluir');
 
 for (let i = 0; i < links.length; i++ ) {
-    links[i].addEventListener("click", function(event) {
-        event.preventDefault();
+links[i].addEventListener("click", function(event) {
+    event.preventDefault();
 
-        let resposta = confirm("Deseja realmente excluir?");
-        if(resposta == true) {
-            location.href = links[i].getAttribute('href');
-        }
-    });
+    let resposta = confirm("Deseja realmente excluir?");
+    if(resposta == true) {
+    location.href = links[i].getAttribute('href');
+    }
+});
 }
